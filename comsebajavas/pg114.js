9282deb9dcc580c2fedce67be9216b3802ffb1a6
JunseoByeon <150450916+Codesignseo@users.noreply.github.com>
2023-12-02 15:41:42 +0900

    Update pg114.js
    Jojosiwa

--- comsebajavas/pg114.js
+++ comsebajavas/pg114.js
@@ -5,7 +5,20 @@
 // alert(bay);
 
 //dec 2nd
-let counter = 1;
-let a = ++counter;
-alert(a);
-alert(counter);
+// let counter = 1;
+// let a = ++counter;
+// alert(a);
+// alert(counter);
+
+// let result = 5 == 5;
+// alert(result);
+
+// let a = 0;
+// alert(Boolean(a));
+// let b = "0";
+// alert(Boolean(b));
+// alert(a === b);
+
+// alert(Boolean(5 > 4));
+// alert(Boolean(undefined == null));
+// alert(Boolean(undefined === null));
