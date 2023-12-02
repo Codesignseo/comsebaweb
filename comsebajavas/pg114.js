1f42b1aad87bffdca5e65f77622c2446a86b6e57
JunseoByeon <150450916+Codesignseo@users.noreply.github.com>
2023-12-02 16:51:37 +0900

    Update pg114.js
    finished up to pg.123

--- comsebajavas/pg114.js
+++ comsebajavas/pg114.js
@@ -22,3 +22,29 @@
 // alert(Boolean(5 > 4));
 // alert(Boolean(undefined == null));
 // alert(Boolean(undefined === null));
+
+// let year = prompt("올해는 몇년도게요?", "");
+// if (year == 2023) {
+//   alert("정답입니다!");
+//   alert("아주 잘했어요!");
+// }
+
+// let year = prompt("올해는 몇년도게?", "");
+// let cond = year == 2022;
+// alert(cond);
+
+// let fucker;
+// let age = prompt("what's your age?", "");
+// if (age < 100) {
+//   fucker = 0;
+// } else if (age > 100) {
+//   fucker = 0;
+// } else {
+//   fucker = 1;
+// }
+// if (fucker) alert("정답입니다!");
+// else alert("nope");
+
+// let age = prompt("Guess if my age is over 18", "");
+// let result = age > 18 ? "정답입니다!" : "오답입니다!";
+// alert(result);
