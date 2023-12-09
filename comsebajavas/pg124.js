5878b2cfcdfc1acc1e2afbe91b3fe1d17d3ca239
JunseoByeon <150450916+Codesignseo@users.noreply.github.com>
2023-12-09 16:33:39 +0900

    december 9th
    coding lesson done

--- comsebajavas/pg124.js
+++ comsebajavas/pg124.js
@@ -1,6 +1,36 @@
-let answer = prompt("자바스크립트의 '공식' 이름은 무었일까요?");
-if (answer === "ECMAScript") {
-  alert("정답입니다!");
-} else {
-  alert("다시 해보세요");
+// let answer = prompt("자바스크립트의 '공식' 이름은 무었일까요?");
+// if (answer === "ECMAScript") {
+//   alert("정답입니다!");
+// } else {
+//   alert("다시 해보세요");
+// }
+
+// alert(0 || 1);  //1을 반환
+
+// if (1 || 0) {
+//   alert("damn");
+// }
+
+// let wrb = prompt("w i y wrb?");
+// if (wrb < 10 || wrb > 19) {
+//   alert("perfect");
+// } else {
+//   alert("no bitch");
+// }
+
+// if (0 && 0) {
+//   alert("응 나와");
+// }
+
+// age = prompt("how old are you?");
+// if (age < 14 || age > 90) {
+//   alert("응 아니야");
+// } else {
+//   alert("속합니다");
+// }
+
+let principal = 51;
+while (principal > 50) {
+  --principal;
+  alert("still fat as fuck");
 }
