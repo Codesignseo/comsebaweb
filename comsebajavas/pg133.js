0718ebe81aae27d74ffe861e21b2e3df12ba9833
JunseoByeon <150450916+Codesignseo@users.noreply.github.com>
2023-12-24 16:36:33 +0900

    12/24 javascript done
    ayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

--- comsebajavas/pg133.js
+++ comsebajavas/pg133.js
@@ -1,18 +1,20 @@
-prime = prompt("type in the number");
-judge = false;
-for (let i = 2; i < prime; i++) {
-  if (prime % i == 0) {
-    judge = true;
-  } else {
-    continue;
-  }
-}
-if (judge) {
-  alert("소수가 아닙니다");
-}
-if (!judge) {
-  alert("소수가 맞습니다");
-}
+// while (1) {
+//   prime = prompt("type in the number");
+//   judge = false;
+//   for (let i = 2; i < prime; i++) {
+//     if (prime % i == 0) {
+//       judge = true;
+//     } else {
+//       continue;
+//     }
+//   }
+//   if (judge) {
+//     alert("소수가 아닙니다");
+//   }
+//   if (!judge) {
+//     alert("소수가 맞습니다");
+//   }
+// }
 
 // alternative
 
@@ -45,17 +47,17 @@ if (!judge) {
 
 //review
 
-let prime = +prompt("rebmun ni epyt");
+// let prime = +prompt("rebmun ni epyt");
 
-for (let i = 2; i < prime; i++) {
-  let isPrime = true;
-  for (let j = 2; j < i; j++) {
-    if (i % j == 0) {
-      isPrime = false;
-      break;
-    }
-  }
-  if (isPrime) {
-    alert(i);
-  }
-}
+// for (let i = 2; i < prime; i++) {
+//   let isPrime = true;
+//   for (let j = 2; j < i; j++) {
+//     if (i % j == 0) {
+//       isPrime = false;
+//       break;
+//     }
+//   }
+//   if (isPrime) {
+//     alert(i);
+//   }
+// }
