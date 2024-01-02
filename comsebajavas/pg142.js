e5dc56270625afb58acfd59cbc94c3e9e291bce6
Junseo <j.qus2046@gmail.com>
2024-01-02 12:53:03 +0900

    1/2 done

--- comsebajavas/pg142.js
+++ comsebajavas/pg142.js
@@ -26,6 +26,34 @@
 // arr.splice(3, 0, "which", "is", "complex"); //그 인덱스 앞에다가 추가한다
 // alert(arr);
 
-let arr = [1, 2, 4];
-arr.splice(-1, 0, 3);
-alert(arr);
+// let arr = [1, 2, 4];
+// arr.splice(-1, 0, 3);
+// alert(arr);
+
+// let arr = [1, 2];
+// let arrayLike = {
+//   0: "something",
+//   length: 1,
+// };
+// alert(arr.concat(arrayLike));
+
+// ["Bilbo", "Gandalf", "Nazgul"].forEach((el) => alert(el));
+
+// [1, 2, 3].forEach((el) => alert(++el));
+
+// ["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
+//   alert(`${item} is at index ${index} in ${array}`);
+// });
+
+// let yes = true;
+// while (yes) {
+//   let kkk = prompt("guess one of the 3 numbers in the list");
+//   let arr = [7, 9, 8];
+//   if (arr.includes(kkk)) {
+//     alert(arr.indexOf(kkk));
+//   } else if (kkk === "thanks") {
+//     yes = false;
+//   } else {
+//     alert("try again");
+//   }
+// }
