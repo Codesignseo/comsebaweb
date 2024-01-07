1ed4e5f718ad1adadcb1e4b208c8bbf056db2a48
JunseoByeon <150450916+Codesignseo@users.noreply.github.com>
2024-01-07 13:14:57 +0900

    lelelee

--- comsebajavas/pg134.js
+++ comsebajavas/pg134.js
@@ -1,4 +1,4 @@
-let browser = prompt("니 컴퓨터 무슨 타입?");
+// let browser = prompt("니 컴퓨터 무슨 타입?");
 // switch (browser) {
 //   case "Edge":
 //     alert("Edge를 사용하고 계시네요!");
@@ -15,12 +15,18 @@ let browser = prompt("니 컴퓨터 무슨 타입?");
 //     alert("현재 페이지가 괜찮아 보이길 바랍니다!");
 // }
 
-//into if...else form
+// //into if...else form
 
-if (browser == "Edge") {
-  alert("Edge를 사용하고 계시네요!");
-} else if (browser == "Chrome" || "Firefox" || "Safari" || "Opera") {
-  alert("저희 서비스가 지원하는 브라우저를 사용하고 계시네요");
-} else {
-  alert("현재 페이지가 괜찮아 보이길 바랍니다!");
-}
+// if (browser == "Edge") {
+//   alert("Edge를 사용하고 계시네요!");
+// } else if (browser == "Chrome" || "Firefox" || "Safari" || "Opera") {
+//   alert("저희 서비스가 지원하는 브라우저를 사용하고 계시네요");
+// } else {
+//   alert("현재 페이지가 괜찮아 보이길 바랍니다!");
+// }
+
+// switch (variable) {
+//   case "compare":
+//     alert("어쩌구저쩌구");
+//     break;
+// }
---
+++ comsebajavas/pg137.js
@@ -0,0 +1,43 @@
+// console.log("pg.137!!");
+
+// let fruits = ["사과", "배", "딸기"];
+// fruits.pop();
+// fruits.push("딸기");
+// fruits.shift();
+// fruits.unshift("사과");
+// fruits.push("오렌지", "파인애플");
+// fruits.unshift("리치", "포도");
+// alert(fruits);
+// // 기댓값 = 리치 포도 사과 배 딸기 오렌지 파인애플
+
+// let arr = ["1", "2", "3"];
+// for (let i = 0; i < arr.length; i++) {
+//   alert(arr[i]);
+// }
+// // 결과: 1,2,3 --> i가 3이 되면 alert전에 if에서 막혀버림
+
+// let fruits = ["사과", "오렌지", "자두"];
+// for (let fruit of fruits) {
+//   alert(fruit);
+// }
+
+// let arr = [];
+// arr[100] = "damn";
+// alert(arr.length);
+
+// let matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// const MATRIX = matrix[0];
+// alert(MATRIX[0]);
+
+let arr = [1, 2, 3, [5, 6], [[[[[[[[[1]]]]]]]]], "3"];
+alert(arr.toString());
+// alert(String(arr) === "1,2,3,5,6,1");
+
+// let fruits = ["사과", "배", "오렌지"];
+// let shoppingCart = fruits;
+// shoppingCart.push("바나나");
+// alert(fruits.length);
